0a396fa1db554933f946b29ca0df9ea0682c6b18
fimaljo <fimlajo.official@gmail.com>
2023-09-18 21:15:41 +0530

    chnages

--- lib/widgets/pagination_widget.dart
+++ lib/widgets/pagination_widget.dart
@@ -4,35 +4,7 @@ import 'package:platform_commons_machine_test/domain/home/models/movie_model.dar
 
 import '../application/home/bloc/home_bloc.dart';
 
-/*
-The PaginationWidget<t> class is a Flutter stateless widget that renders a list of items with pagination support. It takes in several parameters through its constructor to configure its behavior and appearance.
 
-The constructor for the PaginationWidget<t> class has the following parameters:
-
-    loadMore: a function that is called when the user reaches the end of the list and needs to load more items.
-    initialError: a widget to display when an error occurs while loading the initial list of items.
-    initialLoading: a widget to display while the initial list of items is being loaded.
-    initialEmpty: a widget to display when the initial list of items is empty.
-    child: a function that takes an item of type t and returns a widget that represents that item in the list.
-    onLoadMoreError: a widget to display when an error occurs while loading more items.
-    onLoadMoreLoading: a widget to display while more items are being loaded.
-    key: a unique identifier for the widget.
-
-The build method of the PaginationWidget<t> class returns a BlocBuilder widget that listens to the state changes of a ProductsBloc. Based on the current state of the ProductsBloc, the widget will render the appropriate UI components.
-
-If the state is ProductsLoaded, the widget will render a ListView with the items returned by the bloc's products property. The NotificationListener will listen for the end of the list and trigger the loadMore function to load more items if necessary. If an error occurs while loading more items, the onLoadMoreError widget will be displayed. If more items are currently being loaded, the onLoadMoreLoading widget will be displayed.
-
-If the state is ProductsInitialLoading, the initialLoading widget will be displayed.
-
-If the state is ProductsEmpty, the initialEmpty widget will be displayed.
-
-If the state is ProductInitialError, the initialError widget will be displayed.
-
-If none of the above states match the current state of the ProductsBloc, an empty SizedBox will be displayed.
-
-The child function is called for each item in the list to render the appropriate UI components for that item. The type of the item is specified as t in the constructor of the PaginationWidget<t> class.
-
- */
 class PaginationWidget<t> extends StatelessWidget {
   final Function() loadMore;
   final Widget initialError;
